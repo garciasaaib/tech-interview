c0c2eb947e16834542938606446624442d3e90ce
Adrian Garcia <garciasaaib@gmail.com>
2021-10-15 09:49:17 -0600

    ejerciio 2

--- Ejercicios/02-SumArray/SumArray.js
+++ Ejercicios/02-SumArray/SumArray.js
@@ -1,6 +1,12 @@
 function SumArray(arr, n) {
   // Your code here:
-
+  for (var i = 0; i < arr.length; i++) {
+    let current = i + 1
+    for (let j = current; j < arr.length; j++) {
+      if (arr[i] + arr[j] == n) return true
+    }
+  }
+  return false
 }
 
 module.exports = SumArray;
